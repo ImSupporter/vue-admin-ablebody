4360a18406d391067271e760bc7c0acc8b8b376a
ImSupporter <42231194+ImSupporter@users.noreply.github.com>
2022-08-21 18:45:16 +0900

    제안하기 완료

--- src/pages/suggestion-page.vue
+++ src/pages/suggestion-page.vue
@@ -1,12 +1,28 @@
 <template>
+  <!--모달 창-->
+  <div class="modal-background" v-if="modalShown">
+    <div class="modal-main" v-if="modalShown">
+      <h1>{{modal_nickname}}</h1>
+      <p>{{modal_date}}</p>
+      <p>{{modal_content}}</p>
+      <div class="modal-btns">
+        <button style="color: var(--ableblue)">삭제</button>
+        <button @click="modalShown = false" style="background: var(--ableblue); color: var(--white);">확인</button>
+      </div>
+    </div>
+  </div>
+
   <div class="main-wrapper">
     <table class="suggestion-table">
       <thead>
         <th v-for = "(item) in header" v-bind:key="item">{{item}}</th>
       </thead>
       <tbody>
-        <tr v-for = "row in rows" v-bind:key="row">
-          <td v-for = "val in row" v-bind:key="val">{{val}}</td>
+        <tr v-for = "row in rows" v-bind:key="row" @click="this.showModal(row)">
+          <td>{{row.id}}</td>
+          <td>{{row.content}}</td>
+          <td>{{row.nickname}}</td>
+          <td>{{row.createDate}}</td>
         </tr>
       </tbody>
   </table>
@@ -14,110 +30,121 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "SuggestionPage",
     data() {
       return {
+        // 모달
+        modalShown: false,
+        modal_nickname : 'ablebody_official',
+        modal_date : "2022-08-21T11:06:05",
+        modal_content: "운동항목을 커스텀 할 수 있는 기능이 있으면 좋겠어요",
+
+        // 테이블 프로퍼티
         header:["번호", "내용", "작성자", "작성시간"],
-        rows:[
-          {
-            id:'101',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'102',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'103',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'104',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          },
-          {
-            id:'192',
-            content: "애블바디 화이팅하세요",
-            writer: "daa",
-            createDate: "2022-02-01T12:00:00"
-          }
-        ]
+        rows: null
       }
+    },
+    methods: {
+      fetchSuggestions(page){
+        axios.get('/suggestion?page='+page)
+        .then(res=>{
+          this.rows = res.data.data.content
+        })
+      },
+      showModal(suggestion){
+        this.modal_nickname = suggestion.nickname
+        this.modal_date = suggestion.createDate
+        this.modal_content = suggestion.content
+        this.modalShown = true
+      }
+    },
+    created() {
+      this.fetchSuggestions(1)
     },
 }
 </script>
 
 <style>
 @import url(@/assets/colors.css);
+/* 모달 */
+.modal-background{
+  background: #000000A0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.modal-main{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 40px;
+
+  position: relative;
+  width: 50%;
+  height: 40%;
+
+  background: #FFFFFF;
+  box-shadow: 0px 8px 30px rgba(0, 0, 0, 0.12);
+  border-radius: 16px;
+}
+
+.modal-main > h1{
+  font-size : 5.6rem;
+  margin-bottom: 0px;
+}
+.modal-main > :nth-child(2){
+  font-size: 2.4rem;
+  color: var(--small-text-grey);
+  margin: 10px 0;
+}
+.modal-main > :nth-child(3){
+  font-size: 2.0rem;
+  width: 100%;
+  height: 80%;
+  
+  background: var(--plane-grey);
+  display: flex;
+  justify-content: baseline;
+  padding: 5px;
+  border-radius: 5px;
+  overflow: auto;
+}
+.modal-btns{
+  left: 50%;
+  width: 100%;
+  height: 40px;
+
+  display: flex;
+  justify-content: center;
+  gap: 17px;
+}
+.modal-btns button{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  width: 209px;
+  height: 40px;
+  border-radius: 8px;
+  border: 0;
+
+  font-weight: 700;
+}
+
+
+/* 메인 */
 .main-wrapper{
   height: 100%;
   width: 100%;
@@ -178,6 +205,7 @@ th{
 }
 .suggestion-table td:nth-child(4n+2){
   flex: 40 40px;
+  overflow: hidden;
 }
 .suggestion-table td:nth-child(4n+3){
   flex: 20 20px;
