e81d31bba8c2b9c6e16d93f206d76c453cd8fdd8
ImSupporter <42231194+ImSupporter@users.noreply.github.com>
2022-08-22 01:41:39 +0900

    로그인 페이지 추가
     - 로그인 로직 추가해야함

---
+++ src/pages/login-page.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="login-main">
+    <img src="@/assets/login-ci.svg" alt="">
+    <div class="login-form">
+      <div class="id-form">
+        <p>ID</p>
+        <input type="text">
+      </div>
+      <div class="password-form">
+        <p>Password</p>
+        <input type="password" name="" id="">
+      </div>
+      <button>로그인</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+@import url(@/assets/colors.css);
+.login-main{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: baseline;
+  flex-direction: column;
+  gap: 5%;
+}
+.login-main > img{
+  margin-top: 12%;
+  width: 30%;
+}
+.login-form{
+  width: 30%;
+  height: 30%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+.login-form > div{
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  gap: 5px;
+  margin:5px
+}
+.login-form > div >p{
+  font-size: 2.4rem;
+  height: fit-content;
+  line-height: 1;
+  margin: 5px;
+}
+.login-form > div >input{
+  width: 100%;
+  font-size: 3rem;
+  height: fit-content;
+  line-height: 1;
+  margin: 5px;
+
+  background: none;
+  border: 0;
+  border-bottom: 2px solid black;
+  outline: 0;
+
+}
+.id-form{
+  margin-top: 50px;
+}
+
+.login-form > button{
+  background: #0069FF;
+  border-radius: 16px;
+  width: 200px;
+  height: 67px;
+
+  font-size: 3rem;
+  color: var(--white);
+
+  margin-top: 30px;
+  outline: 0;
+  border: 0;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -5,33 +5,41 @@ import UserPage from './pages/user-page';
 import QnaPage from './pages/qna-page';
 import ReportPage from './pages/report-page';
 import SuggesstionPage from './pages/suggestion-page';
+import LonginPage from '@/pages/login-page'
 
 // 라우트 정보
 // 라우팅을 해주면서 #을 제거해주면서, path에 입력한 경로에 매핑된 컴포넌트를 보여준다.
 const route = [
-    {path: "/", component: DashboardLayout,
-     children:[
-        {
-            path: 'user',
-            component: UserPage,
-            name : '유저 관리'
-        },
-        {
-            path: 'qna',
-            component: QnaPage,
-            name : '운동생활 관리'
-        },
-        {
-            path: 'report',
-            component: ReportPage,
-            name : '신고하기'
-        },
-        {
-            path: 'suggestion',
-            component: SuggesstionPage,
-            name : '제안하기'
-        }
-     ]}
+    {
+        path: "/", 
+        component: DashboardLayout,
+        children:[
+            {
+                path: 'user',
+                component: UserPage,
+                name : '유저 관리'
+            },
+            {
+                path: 'qna',
+                component: QnaPage,
+                name : '운동생활 관리'
+            },
+            {
+                path: 'report',
+                component: ReportPage,
+                name : '신고하기'
+            },
+            {
+                path: 'suggestion',
+                component: SuggesstionPage,
+                name : '제안하기'
+            }
+        ]
+    },
+    {
+        path: '/login',
+        component: LonginPage
+    }
 ];
 
 // Vue 라우터 인스턴스 생성
