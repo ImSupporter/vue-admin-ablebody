aed9311aac8f8103877d69381e33fa80b35a4aae
ImSupporter <42231194+ImSupporter@users.noreply.github.com>
2022-09-07 23:34:06 +0900

    인터셉터 버그 수정

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -27,7 +27,7 @@ export function setInterceptors(instance) {
     function(error) {
       // Any status codes that falls outside the range of 2xx cause this function to trigger
       // Do something with response error
-      if(error.response && error.status === 403){
+      if(error.response && error.response.status === 403){
         store.dispatch('authFail')
         router.push('/login')
         return;
